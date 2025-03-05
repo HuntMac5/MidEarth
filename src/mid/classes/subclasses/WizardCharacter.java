52992829296d33286582b03e230709df386e949a
HuntMac5 <huntermccall.hm@gmail.com>
2025-03-05 16:04:31 -0600

    renaming the conditions of attack methods

--- src/mid/classes/subclasses/WizardCharacter.java
+++ src/mid/classes/subclasses/WizardCharacter.java
@@ -7,20 +7,21 @@ public class WizardCharacter extends MiddleEarthCharacter{
 	@Override
 	public boolean attack(MiddleEarthCharacter target) {
 		// 1.5x damage against DWARF
-		// 0x damage against HUMAN WIZARD
-		// 1x damage against ELF ORC
+		// 0x damage against HUMAN & WIZARD
+		// 1x damage against ELF & ORC
 		double hp = 0; 
+		System.out.println(this.getName() + " is attacking...");
 		
 		if( target instanceof DwarfCharacter){
 			hp = 1.5*this.getPower();
 			hp = target.getHealth()-hp;
 			target.setHealth(hp);
 			
-			System.out.println("Attack Successful! ");
+			System.out.println(this.getName() + "");
 			return true;}
 		
 		if( target instanceof HumanCharacter  | target instanceof WizardCharacter ){
-			System.out.println("Attack Unsuccessful... ");
+			System.out.println("The attack was unsuccessful... ");
 			return false;}
 		
 		if( target instanceof ElfCharacter  | target instanceof OrcCharacter ){
